eb22c698a1fb6efc7bb0c9c2a0dfa7b9a307ebc0
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-08-26 18:14:09 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,15 @@
+a=" January "
+b="February"
+c="March"
+d="April"
+e="May"
+f="June"
+g="July"
+h="August"
+i="Setember"
+j="October"
+k="November"
+l="December"
+date=12 
+year= 2011
+print(str("Your birthday is at ")+str(date)+str(a)+str(year))
